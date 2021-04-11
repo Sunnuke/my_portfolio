4e9eabaed6ff86577c8624b093f181a5dd38f0ae
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-04-11 14:04:39 -0700

    Fixed bug that caused mixed http content

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,8 +12,13 @@ export default class Porfolio extends Component {
             resumeData.portfolio && resumeData.portfolio.map((item)=>{
               return(
                 <div className="columns portfolio-item">
+                  <h5 style={{ color: "#FF00FF", textShadow: "0px 0px 5px #FF00FF" }}>
+                    Check out <a style={{ color: "#00FFFF", textShadow: "0px 0px 3px #00FFFF" }} href={`${item.site}`} target="_blank" rel="noopener noreferrer">
+                      {item.name}</a>
+                    now!
+                  </h5>
                   <div className="item-wrap">
-                    <a href={`${item.site}`} target="_blank" rel="noopener noreferrer">
+                    {/* <a href={`${item.site}`} target="_blank" rel="noopener noreferrer"> */}
                       <img src={`${item.imgurl}`} className="item-img"/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">

0eba27cb2bfd6dc7d7c83a9f877a47a73c1581cb
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-04-11 11:54:14 -0700

    Created a React app and published it to GitHub Pages

---
+++ src/resumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Javier Rivera",
+    "role": "Full Stack Web Developer, tech-savvy, cyberpunk fan boy, and veteran",
+    "linkedinId":" https://linkedin.com/in/javier-c-rivera",
+    "twitterId": "https://twitter.com/sunnuke",
+    "roleDescription": " I have basic knowledge and experience with Full Stack Development using Python, JAVA, MERN and I am passionate about the puzzles of creation with computer technology. Also I like to learn about new technologies, and solve puzzle cubes or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://linkedin.com/in/javier-c-rivera",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Sunnuke",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/sunnuke",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":'I am a Coding Dojo coding bootcamp graduate and a military veteran with 3 years of expertise in naval logistics. I look forward to developing and delivering meaningful web applications and products that can help companies develop solutions to improve user experiences. My Motto is: "Never Believe that you are the Best, because you can always be Better." I say this because there is always room for improvement.',
+    "address":"San Diego, California",
+    "website":"https://linkedin.com/in/javier-c-rivera",
+    "email":" javiercr3xx@gmail.com",
+    "education":[
+      {
+        "UniversityName":"The LNM Insitute of Information Technology",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Xplanet",
+        "description":"A Synthwave Styled social platform concept for gamers, enabling users to access curated gaming news and media, interact with other gamers, create and personalized private profile pages",
+        "imgurl":"images/portfolio/Xplanet.gif"
+      },
+      {
+        "name":"PokeDexAPI",
+        "description":"An app for Pokemon fans that enables users to explore the entire library of the Pokemon universe and see their stats and alternate versions.",
+        "imgurl":"images/portfolio/Pokedex.gif"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData

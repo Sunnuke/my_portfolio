df6cdd989f18dd032b5393671a752227dce06e6f
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-04-11 12:58:42 -0700

    Added anchor attributes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,7 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href={`${item.site}`}>
+                    <a href={`${item.site}`} target="_blank" rel="noopener noreferrer">
                       <img src={`${item.imgurl}`} className="item-img"/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
